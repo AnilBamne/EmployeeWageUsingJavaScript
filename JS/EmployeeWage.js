a10ff580464cc988cf3b8767560b37d3e4d54bb0
AnilBamne <anilbamne20@gmail.com>
2022-12-27 23:23:59 +0530

    Adding:UC1CheckEmployeeIsPresentOrAbsent code

--- JS/EmployeeWage.js
+++ JS/EmployeeWage.js
@@ -1,2 +1,9 @@
 /// Welcome To Employe wage program ///
 console.log("Welcome To Employe wage program using JS");
+/// uc1 - Check employee is present or not ///
+let present=1;
+let empInput=Math.floor((Math.random()*10)%2);
+if(empInput==present)
+    console.log("Employee is present")
+else
+    console.log("Employee is absent");
