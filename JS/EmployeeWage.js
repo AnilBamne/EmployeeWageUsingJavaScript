00bb9c43a67611b124a0cdb96eddd0cf60c45d64
AnilBamne <anilbamne20@gmail.com>
2022-12-29 06:47:33 +0530

    Adding:UC4ComputeTotalWageForAMonth code

--- JS/EmployeeWage.js
+++ JS/EmployeeWage.js
@@ -1,12 +1,14 @@
 /// Welcome To Employe wage program ///
 console.log("Welcome To Employe wage program using JS");
 /// uc1 - Check employee is present or not ///
-let IS_FULL_TIME=1;
-let IS_PART_TIME=2;
-let EMP_RATE_PER_HR=20;
+const IS_FULL_TIME=1;
+const IS_PART_TIME=2;
+const EMP_RATE_PER_HR=20;
+const MAX_WORKING_DAYS=20;
+
 let empHrs=0;
-let empWage=0;
-let empInput=Math.floor((Math.random()*10)%3);
+let totalEmpHrs=0;
+let empWage=0,totalEmpWage=0;
 
 /// Calculate daily eage using Function
 function GetEmpHours(empInput)
@@ -27,5 +29,13 @@ function GetEmpHours(empInput)
             return empHrs=0;
     }
 }
-empWage=EMP_RATE_PER_HR*GetEmpHours(empInput);
-console.log("By function Employee wage is:",empWage);
+
+for(var day=0;day<=MAX_WORKING_DAYS;day++)
+{
+    let empInput=Math.floor((Math.random()*10)%3);
+    empWage=EMP_RATE_PER_HR*GetEmpHours(empInput);
+    console.log("day "+day+" wage is "+(empHrs*EMP_RATE_PER_HR))
+    totalEmpHrs+=empHrs;
+}
+totalEmpWage=totalEmpHrs*EMP_RATE_PER_HR;
+console.log("Total Employee wage is:",totalEmpWage);
