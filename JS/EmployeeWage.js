367455aec8ede60fc4c0213456bd65178b3c9d74
AnilBamne <anilbamne20@gmail.com>
2022-12-29 06:59:46 +0530

    Adding:UC4ComputeTotalWageTillMaxWorkingDaysOrHrsReached code

--- JS/EmployeeWage.js
+++ JS/EmployeeWage.js
@@ -5,9 +5,10 @@ const IS_FULL_TIME=1;
 const IS_PART_TIME=2;
 const EMP_RATE_PER_HR=20;
 const MAX_WORKING_DAYS=20;
+const MAX_WORKING_HRS=160;
 
 let empHrs=0;
-let totalEmpHrs=0;
+let totalEmpHrs=0,day=0;
 let empWage=0,totalEmpWage=0;
 
 /// Calculate daily eage using Function
@@ -17,25 +18,26 @@ function GetEmpHours(empInput)
     switch(empInput)
     {
         case IS_FULL_TIME:
-            console.log("Employee present full time");
+            //console.log("Employee present full time");
             return empHrs=8;
             
         case IS_PART_TIME:
-            console.log("Employee present part time");
+            //console.log("Employee present part time");
             return empHrs=4;
             
         default:
-            console.log("Employee is absent");
+            //console.log("Employee is absent");
             return empHrs=0;
     }
 }
 
-for(var day=0;day<=MAX_WORKING_DAYS;day++)
+while(day<=MAX_WORKING_DAYS && totalEmpHrs<=MAX_WORKING_HRS)
 {
     let empInput=Math.floor((Math.random()*10)%3);
     empWage=EMP_RATE_PER_HR*GetEmpHours(empInput);
-    console.log("day "+day+" wage is "+(empHrs*EMP_RATE_PER_HR))
+    //console.log("day "+day+" wage is "+(empHrs*EMP_RATE_PER_HR))
     totalEmpHrs+=empHrs;
+    day++;
 }
 totalEmpWage=totalEmpHrs*EMP_RATE_PER_HR;
-console.log("Total Employee wage is:",totalEmpWage);
+console.log("Total Employee wage for "+(day-1)+" days or "+(totalEmpHrs)+" working Hrs is:"+totalEmpWage);
